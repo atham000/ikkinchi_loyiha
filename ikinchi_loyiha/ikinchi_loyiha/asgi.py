04c1dacadaeb1a2aca616826af618b2d8189056d
atham000 <athambekortiqboyev@gmail.com>
2022-05-18 18:19:30 +0500

    ikkinchi loyiha

---
+++ ikinchi_loyiha/ikinchi_loyiha/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ikinchi_loyiha project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ikinchi_loyiha.settings')
+
+application = get_asgi_application()
